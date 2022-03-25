cf154c0f2ddc6055a1112f35766f47ac2bcffbfa
UD-sht <udshrestha48@gmail.com>
2022-03-25 17:39:29 +0545

    commit

--- script_1.js
+++ script_1.js
@@ -1,53 +1,51 @@
-
 let searchbtn = document.querySelector('#search-btn');
-let searchbar = document.querySelector('.search-bar-container');
-
-window.onscroll = () =>{
-    searchbtn.classList.remove('fa-times');
-    searchbar.classList.remove('active');
-}
-
-searchbtn.addEventListener('click', () =>{
-    searchbtn.classList.toggle('fa-times');
-    navbar.classList.remove('active');
-    searchbar.classList.toggle('active');
-});
-
-let userbtn = document.querySelector('#user-btn');
-let loginbox = document.querySelector('.login-form-container');
-
-userbtn.addEventListener
+let searchcontainer = document.querySelector('.search-container');
+searchbtn.addEventListener
 (
     'click', () =>
     {
-        loginbox.classList.toggle('active');
-        navbar.classList.remove('active');
-        searchbar.classList.remove('active');
-        searchbtn.classList.remove('fa-times');
+        searchbtn.classList.toggle('fa-times');
+        searchcontainer.classList.toggle('active');
+        navul.classList.remove('active');
     }
 );
-
-let closeform = document.querySelector('#close-form');
-closeform.addEventListener
-(
-    'click',() =>
-    {
-        loginbox.classList.remove('active');
-    }
-);
-
-let menubars = document.querySelector('#menu-bars');
-let navbar = document.querySelector('nav ul');
-
-menubars.addEventListener
-(
-    'click',() =>
-    {
-        navbar.classList.toggle('active');
-        searchbar.classList.remove('active');
-        searchbtn.classList.remove('fa-times');
-    }
-);
-window.onscroll = () =>{
-    navbar.classList.remove('active');
+window.onscroll, () =>
+{
+    searchbtn.classList.remove('fa-times');
+    searchcontainer.classList.remove('active');  
+    navul.classList.remove('active'); 
 }
+let userlogin = document.querySelector('#user-btn');
+let logincontainer = document.querySelector('.login-container');
+userlogin.addEventListener
+(
+    'click', () =>
+    {
+        logincontainer.classList.toggle('active');
+        searchbtn.classList.remove('fa-times');
+        searchcontainer.classList.remove('active'); 
+        navul.classList.remove('active');
+    }
+);
+let closelogin = document.querySelector('#close-login');
+closelogin.addEventListener
+(
+    'click', () =>
+    {
+        logincontainer.classList.remove('active');
+    }
+);
+
+let menubar = document.querySelector('#menu-bars');
+let navul = document.querySelector('nav ul');
+
+menubar.addEventListener
+(
+    'click', () =>
+    {
+        navul.classList.toggle('active');
+        searchbtn.classList.remove('fa-times');
+        searchcontainer.classList.remove('active'); 
+        logincontainer.classList.remove('active');
+    }
+);
